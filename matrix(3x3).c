3b0922a4b1ccd69ef0b79c671f6eafba8045f1e2
vis-hwa26 <99121647+vis-hwa26@users.noreply.github.com>
2022-03-23 18:26:35 +0530

    reading ,printing and sum of diagonal elements

---
+++ matrix(3x3).c
@@ -0,0 +1,39 @@
+// 3x3 Matrices,then read and print an array of elements .
+//https://github.com/vis-hwa26/C-programming//
+#include<stdio.h>
+main()
+{
+	//Declaration of array.
+	int a[3][3],i,j;
+	int *p=&a;
+	//Taking inputs from the user.
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("element - [%d],[%d] : ",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	//Reading and Printing the array.
+	printf("\nThe matrix is : \n");
+	for(i=0;i<3;i++)
+	{
+        printf("\n");
+        for(j=0;j<3;j++)
+           printf("\t%d\t",a[i][j]);
+	}
+	//sum of the diagonal elements. 
+	int sum=1;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			if (i=j)
+				{
+					sum=sum+a[i][j];
+			    }
+		}
+	}
+	printf("\nThe sum of diagonal elements : %d",sum);
+}
